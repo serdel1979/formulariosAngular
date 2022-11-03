5befe6abb09c9b2c3b92dadcb8f52ec10e7e1990
Sergio De Luca <sdlbsso@gmail.com>
2022-11-03 16:45:02 -0300

    Formularios dinamicos con angular

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,25 +1,65 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+
+
+interface Persona{
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito{
+  id: number;
+  nombre: string;
+}
+
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styleUrls: ['./dinamicos.component.css']
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
+
+
+  nfavorito!: string;
+
+
+  persona: Persona ={
+    nombre: 'Rick',
+    favoritos: [
+      {
+        id: 1,
+        nombre: 'Synfo'
+      },
+      {
+        id: 2,
+        nombre:'Colours'
+      }
+    ]
+  }
 
   constructor() { }
 
   @ViewChild('miFormulario') miFormulario!: NgForm;
 
 
-  ngOnInit(): void {
-  }
+ 
 
   guardar(){
-    console.log(this.miFormulario);
+
   }
 
- 
+  agregar(){
+    const nuevo: Favorito = {
+      id : this.persona.favoritos.length+1,
+      nombre : this.nfavorito
+    }
+    this.persona.favoritos.push({...nuevo});
+    this.nfavorito = "";
+  }
+
+  eliminar(indice: number){
+    this.persona.favoritos.splice(indice,1);
+  }
 
 }
