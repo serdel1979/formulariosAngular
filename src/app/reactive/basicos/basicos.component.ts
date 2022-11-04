047627ad2336aeed54a51218b5a1301e7b3d7445
Sergio De Luca <sdlbsso@gmail.com>
2022-11-04 18:06:56 -0300

    formularios reactivos

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,17 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
   styleUrls: ['./basicos.component.css']
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  //miFormulario: FormGroup = new FormGroup({
+  //  'nombre': new FormControl('RTX 4080TI'),
+  //  'precio':  new FormControl(1500),
+  //  'existencias':  new FormControl(5)
+  //})
 
+  miFormulario: FormGroup = this.fb.group({
+    'nombre': ['RTX 4080 TI'],
+    'precio': [5],
+    'existencias': [10]
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+ 
 
 
  
