9af7b8dc7c52145ab0a1ddff362397f6a40dcbc5
Sergio De Luca <sdlbsso@gmail.com>
2022-11-06 15:30:24 -0300

    formulario de registro

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  nompreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre:['',[Validators.required, Validators.pattern(this.nompreApellidoPattern)]]
+  })
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  campoNoValido(campo:string){
+    return this.miFormulario.get(campo)?.invalid && this.miFormulario.get(campo)?.touched
+  }
+
+  submitForm(){
+    console.log(this.miFormulario.value);
+
+    this.miFormulario.markAllAsTouched();
+  }
 }
