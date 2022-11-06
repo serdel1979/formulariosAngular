eea71877d76da4571a0d54f54dde538cc7717689
Sergio De Luca <sdlbsso@gmail.com>
2022-11-06 01:52:03 -0300

    formularios reactivos

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,6 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+
+  miFormulario: FormGroup = this.fb.group({
+    genero: ['M', Validators.required],
+    notificaciones: [true, Validators.required],
+    terminos: [false, Validators.requiredTrue]
+  });
+
+  persona = {
+    genero: 'F',
+    notificaciones: false
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+      this.miFormulario.reset({
+        ...this.persona,
+        condiciones: false
+      });
+
+
+      this.miFormulario.get('terminos')?.valueChanges.subscribe(campo=>{
+        console.log(campo)
+      })
+
+      this.miFormulario.valueChanges.subscribe(form=>{
+        console.log(form)
+      })
+
+  }
+  
+  guardar(){
+    const formValue = {...this.miFormulario.value};
+    delete formValue.terminos
+
+    console.log(formValue);
   }
 
 }
