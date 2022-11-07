c10b53bf1bc2a55945f5f3f5c77ca6c2467bc6d3
Sergio De Luca <sdlbsso@gmail.com>
2022-11-07 15:46:38 -0300

    registro

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -9,14 +9,34 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class RegistroComponent implements OnInit {
 
   nompreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+  noPuedeSerStrider(control: FormControl){
+    const valor = control.value?.trim().toLowerCase();
+    if(valor === 'strider'){
+      return {
+        noStrider: true
+      }
+    }
+     return null;
+  }
+
+
 
   miFormulario: FormGroup = this.fb.group({
-    nombre:['',[Validators.required, Validators.pattern(this.nompreApellidoPattern)]]
+    nombre:['',[Validators.required, Validators.pattern(this.nompreApellidoPattern)]],
+    email:['',[Validators.required, Validators.pattern(this.emailPattern)]],
+    username:['',[Validators.required, this.noPuedeSerStrider]]
   })
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre:'Sergio DeLuca',
+      email: 'test1@test.com',
+      username: 'El_enzo'
+    })
   }
 
   campoNoValido(campo:string){
