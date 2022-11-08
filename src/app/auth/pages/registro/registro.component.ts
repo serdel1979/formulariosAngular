8600a0b86b74d817ddd9b1f1d2992c6a92b54ef0
Sergio De Luca <sdlbsso@gmail.com>
2022-11-08 17:34:11 -0300

    formulario registro

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+//import { emailPattern, nompreApellidoPattern, noPuedeSerStrider } from 'src/app/shared/validaciones';
+import { ValidatorService } from '../../../shared/validator/validator.service';
 
 @Component({
   selector: 'app-registro',
@@ -8,28 +10,24 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
 })
 export class RegistroComponent implements OnInit {
 
-  nompreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
-  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+ 
 
-  noPuedeSerStrider(control: FormControl){
-    const valor = control.value?.trim().toLowerCase();
-    if(valor === 'strider'){
-      return {
-        noStrider: true
-      }
-    }
-     return null;
-  }
+
 
 
 
   miFormulario: FormGroup = this.fb.group({
-    nombre:['',[Validators.required, Validators.pattern(this.nompreApellidoPattern)]],
-    email:['',[Validators.required, Validators.pattern(this.emailPattern)]],
-    username:['',[Validators.required, this.noPuedeSerStrider]]
+    nombre:['',[Validators.required, Validators.pattern(this.validatorService.nombreApellidoPattern)]],
+    email:['',[Validators.required, Validators.pattern(this.validatorService.emailPattern)]],
+    username:['',[Validators.required, this.validatorService.noPuedeSerStrider]],
+    password:['',[Validators.required, Validators.minLength(6)]],
+    passwordConfirm:['',[Validators.required]],
+  },{
+     validators:[this.validatorService.camposIguales('password','passwordConfirm')] 
   })
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, 
+    private validatorService: ValidatorService) { }
 
   ngOnInit(): void {
     this.miFormulario.reset({
